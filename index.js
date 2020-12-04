3ea3bc4c3a4b79a74edd21dc0351e00b7ddad694
Dart000 <75501448+Dart000@users.noreply.github.com>
2020-12-04 17:13:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var options =
 		username: "dart_gamesyt",
 		password: "oauth:s1j8myaq0jpz628tv3w9jzgzg4xwdp",  
 	},
-	channels: ['#gaules', '#inr1tv', '#marcobc', '#murilo_rt', '#jeffaocs', '#pescocofino', '#csrfps', '#mibrtv', '#marcaorx', '#sheviiioficial', '#brnwowzk1', '#gabepeixe', '#stev3z', '#Vovo', '#furiatv', '#narigafps', '#stereonline', '#skipnho', '#skipnholive', '#saullo', '#mch_agg', '#sterinrr', '#nobru', ] //ADICIONA OS CANAIS AQ COM '#NOME'
+	channels: ['#gaules', '#inr1tv', '#marcobc', '#murilo_rt', '#jeffaocs', '#pescocofino', '#csrfps', '#mibrtv', '#marcaorx', '#sheviiioficial', '#brnwowzk1', '#gabepeixe', '#stev3z', '#Vovo', '#furiatv', '#narigafps', '#stereonline', '#skipnho', '#skipnholive', '#saullo', '#mch_agg', '#sterinrr', '#nobru', '#deercherup', ] //ADICIONA OS CANAIS AQ COM '#NOME'
 };
 
 var client = new tmi.client(options);
